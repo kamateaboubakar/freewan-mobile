3b7dbc7b34ff3c8d9c0235ff8f0a3f575c933ac0
amangoua01 <64694144+amangoua01@users.noreply.github.com>
2023-04-09 17:01:23 +0000

    Initial commmit

---
+++ android/app/src/main/kotlin/com/example/wan_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wan_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'views/static/splashscreen.dart';
+
+void main() {
+  runApp(
+    const GetMaterialApp(
+      home: SplashScreen(),
+    ),
+  );
+}

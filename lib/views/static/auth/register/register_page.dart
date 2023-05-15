c5e0d25b01d17f3837185dd3016cf7888c6b2114
amangoua01 <64694144+amangoua01@users.noreply.github.com>
2023-05-15 14:44:24 +0000

    save du bouton continue de la page register

--- lib/views/static/auth/register/register_page.dart
+++ lib/views/static/auth/register/register_page.dart
@@ -16,72 +16,73 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<RegisterPageVctl>(
-        init: RegisterPageVctl(),
-        builder: (ctl) {
-          return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              backgroundColor: Colors.transparent,
-              foregroundColor: Colors.black,
-            ),
-            bottomNavigationBar: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              child: CButton(
-                textColor: const Color.fromRGBO(18, 43, 84, 1),
-                minWidth: double.infinity,
-                height: 48,
-                onPressed: () => Get.to(
-                  () => const HomePage(
-                    displayWelcome: true,
-                  ),
+      init: RegisterPageVctl(),
+      builder: (ctl) {
+        return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.black,
+          ),
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: CButton(
+              textColor: Colors.white,
+              minWidth: double.infinity,
+              height: 48,
+              onPressed: () => Get.to(
+                () => const HomePage(
+                  displayWelcome: true,
                 ),
-                child: const Text(
-                  "Continue",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+              ),
+              child: const Text(
+                "Continue",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Image.asset("assets/images/logo_bedoo.png",
-                          width: 94, height: 43),
-                      SizedBox(
-                        width: 50,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(5),
-                          child: Text(
-                            "${ctl.currentStep}/6",
-                          ),
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset("assets/images/logo_bedoo.png",
+                        width: 94, height: 43),
+                    SizedBox(
+                      width: 50,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Text(
+                          "${ctl.currentStep}/6",
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Expanded(
-                  child: PageView(
-                    controller: ctl.pageController,
-                    // physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      InfosPersoRegister(ctl),
-                      EmailRegister(ctl),
-                      SecurityQuestionRegister(ctl),
-                      PasswordRegister(ctl),
-                      ConfirmPasswordRegister(ctl),
-                      ServiceRegister(ctl),
-                    ],
-                  ),
+              ),
+              Expanded(
+                child: PageView(
+                  controller: ctl.pageController,
+                  // physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    InfosPersoRegister(ctl),
+                    EmailRegister(ctl),
+                    SecurityQuestionRegister(ctl),
+                    PasswordRegister(ctl),
+                    ConfirmPasswordRegister(ctl),
+                    ServiceRegister(ctl),
+                  ],
                 ),
-              ],
-            ),
-          );
-        });
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }

42390666e919bc8fadbe7eb593f3767c75021709
amangoua01 <64694144+amangoua01@users.noreply.github.com>
2023-05-20 19:23:52 +0000

    ajout d'un padding dans la liste des campagnes du user

--- lib/views/static/don/user_campagne_page.dart
+++ lib/views/static/don/user_campagne_page.dart
@@ -18,6 +18,7 @@ class UserCampagnePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: WrapperBodyListView(
+        listPadding: const EdgeInsets.all(10),
         loading: false,
         refreshButton: COutlinedButton(
           icon: const Icon(Icons.add),

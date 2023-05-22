c67789a91fbc6d85bbd8765c6462a33b495c6f5c
agnamc <agnaramonboris@gmail.com>
2023-05-22 01:25:03 +0000

    implementation webcontroller pour gaz

---
+++ lib/api/controllers/gaz_api_ctl.dart
@@ -0,0 +1,104 @@
+import 'package:wan_mobile/models/gas_size.dart';
+import 'package:wan_mobile/models/shop.dart';
+import 'package:wan_mobile/tools/types/types.dart';
+
+import '../../models/pays.dart';
+import '../../tools/const/const.dart';
+import '../../tools/utils/http_response.dart';
+import '../abstracts/http_client_const.dart';
+import '../abstracts/web_controller.dart';
+
+class GazApiCtl extends WebController {
+  Future<HttpResponse<List<Shop>>> closestShop(
+      {required double latitude, required double longitude}) async {
+    try {
+      var res = await get(
+          "${Const.gazBaseUrl}/shops/closests?latitude=$latitude&longitude=$longitude",
+          headers: HttpClientConst.headers);
+      var body = HttpResponse.decodeBody(res);
+
+      if (body.status) {
+        return HttpResponse.success(
+            data: (body.data as List).map((e) => Shop.fromJson(e)).toList());
+      } else {
+        return HttpResponse.error(message: body.message);
+      }
+    } catch (e) {
+      return HttpResponse.error(detailErrors: e.toString());
+    }
+  }
+
+  Future<HttpResponse<Shop>> getShopById({required int id}) async {
+    try {
+      var res = await get("${Const.gazBaseUrl}/shops/$id",
+          headers: HttpClientConst.headers);
+      var body = HttpResponse.decodeBody(res);
+
+      if (body.status) {
+        return HttpResponse.success(data: Shop.fromJson(body.data));
+      } else {
+        return HttpResponse.error(message: body.message);
+      }
+    } catch (e) {
+      return HttpResponse.error(detailErrors: e.toString());
+    }
+  }
+
+  Future<HttpResponse<List<GasSize>>> getGasSize({
+    required int shopId,
+    required int brandId,
+  }) async {
+    try {
+      var res = await get(
+          "${Const.gazBaseUrl}/brandsInShop/gasSizes?shop_id=$shopId&brand_id=$brandId",
+          headers: HttpClientConst.headers);
+      var body = HttpResponse.decodeBody(res);
+
+      if (body.status) {
+        return HttpResponse.success(
+            data: (body.data as List).map((e) => GasSize.fromJson(e)).toList());
+      } else {
+        return HttpResponse.error(message: body.message);
+      }
+    } catch (e) {
+      return HttpResponse.error(detailErrors: e.toString());
+    }
+  }
+
+  Future<HttpResponse<bool>> submitOrder({
+    required String brandInShopId,
+    required String customerSessionId,
+    required String customerAddressId,
+    required String latitude,
+    required String longitude,
+    required String targetId,
+    required String price,
+  }) async {
+    try {
+      var res = await post(
+        HttpClientConst.baseUrl(module: "challenge-otp"),
+        {
+          "brandInShopId": brandInShopId,
+          "customerSessionId": customerSessionId,
+          "customerAddressId": customerAddressId,
+          "latitude": latitude,
+          "longitude": longitude,
+          "targetId": targetId,
+          "price": price,
+        }.toJson(),
+        headers: HttpClientConst.headers,
+      );
+      var body = HttpResponse.decodeBody(res);
+      if (body.status) {
+        if (body.data != null) {
+          return HttpResponse.success(data: true);
+        }
+        return HttpResponse.success(data: false);
+      } else {
+        return HttpResponse.error(message: body.message);
+      }
+    } catch (e, st) {
+      return HttpResponse.error(systemError: e, systemtraceError: st);
+    }
+  }
+}
---
+++ lib/models/shop.dart
@@ -0,0 +1,100 @@
+class Shop {
+  String? availableWorkDays;
+  String? description;
+  String? availableWeekEndTime;
+  String? availableWorkBeginTime;
+  double? latitude;
+  String? address;
+  String? availableWorkEndTime;
+  String? availableWeekBeginTime;
+  String? availableWeekDays;
+  int? id;
+  String? name;
+  double? longitude;
+  int? open;
+  List<Brands>? brands;
+
+  Shop(
+      {this.availableWorkDays,
+        this.description,
+        this.availableWeekEndTime,
+        this.availableWorkBeginTime,
+        this.latitude,
+        this.address,
+        this.availableWorkEndTime,
+        this.availableWeekBeginTime,
+        this.availableWeekDays,
+        this.id,
+        this.name,
+        this.longitude,
+        this.open,
+        this.brands});
+
+  Shop.fromJson(Map<String, dynamic> json) {
+    availableWorkDays = json['available_work_days'];
+    description = json['description'];
+    availableWeekEndTime = json['available_week_end_time'];
+    availableWorkBeginTime = json['available_work_begin_time'];
+    latitude = json['latitude'];
+    address = json['address'];
+    availableWorkEndTime = json['available_work_end_time'];
+    availableWeekBeginTime = json['available_week_begin_time'];
+    availableWeekDays = json['available_week_days'];
+    id = json['id'];
+    name = json['name'];
+    longitude = json['longitude'];
+    open = json['open'];
+    if (json['brands'] != null) {
+      brands = <Brands>[];
+      json['brands'].forEach((v) {
+        brands!.add(new Brands.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['available_work_days'] = this.availableWorkDays;
+    data['description'] = this.description;
+    data['available_week_end_time'] = this.availableWeekEndTime;
+    data['available_work_begin_time'] = this.availableWorkBeginTime;
+    data['latitude'] = this.latitude;
+    data['address'] = this.address;
+    data['available_work_end_time'] = this.availableWorkEndTime;
+    data['available_week_begin_time'] = this.availableWeekBeginTime;
+    data['available_week_days'] = this.availableWeekDays;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['longitude'] = this.longitude;
+    data['open'] = this.open;
+    if (this.brands != null) {
+      data['brands'] = this.brands!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Brands {
+  String? description;
+  int? id;
+  String? pictureName;
+  String? name;
+
+  Brands({this.description, this.id, this.pictureName, this.name});
+
+  Brands.fromJson(Map<String, dynamic> json) {
+    description = json['description'];
+    id = json['id'];
+    pictureName = json['picture_name'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['description'] = this.description;
+    data['id'] = this.id;
+    data['picture_name'] = this.pictureName;
+    data['name'] = this.name;
+    return data;
+  }
+}

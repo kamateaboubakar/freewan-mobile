29f051b8a7a48c1ee01d9a1c94bb51e2861926e4
Yves AMANGOUA <mr.amangoua@gmail.com>
2024-03-17 21:21:01 +0000

    ajout du module loyer et suppression des fichiers présent dans le projet concernant loyer

--- lib/views/static/loyer/loyer_bottom_sheet.dart
+++ lib/views/static/loyer/loyer_bottom_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lebedoo_assets/lebedoo_assets.dart';
-import 'package:wan_mobile/views/static/loyer/locataire/formulaire_code_maison_loyer.dart';
-import 'package:wan_mobile/views/static/loyer/proprio/bien_immobilier_page.dart';
+import 'package:lebedoo_loyer/views/static/locataire/loyer_locataire_home_page.dart';
+import 'package:lebedoo_loyer/views/static/proprietaire/loyer_proprietaire_home_page.dart';
 
 class LoyerBottomSheet extends StatelessWidget {
   const LoyerBottomSheet({super.key});
@@ -14,79 +14,49 @@ class LoyerBottomSheet extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(15),
+          ListTile(
+            leading:
+                Image.asset("assets/images/icons/proprio_logo.png", width: 48),
+            title: const Text(
+              "Propriétaire / Manager",
+              style: TextStyle(
+                fontSize: 15,
+                color: Color.fromRGBO(7, 21, 60, 1),
               ),
             ),
-            height: 249,
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 17, bottom: 13),
-                  child: Text(
-                    "Choisissez votre profil",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                const Divider(
-                  thickness: 1,
-                  color: Color.fromRGBO(181, 196, 216, 0.65),
-                ),
-                ListTile(
-                  leading: Image.asset(
-                    "assets/images/icons/proprio_logo.png",
-                    width: 48,
-                    height: 48,
-                  ),
-                  title: const Text(
-                    "Propriétaire / Manager",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color.fromRGBO(7, 21, 60, 1),
-                    ),
-                  ),
-                  subtitle: const Text(
-                    "Vous possédez ou gérez 1 ou plusieurs biens immobilliers",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Color.fromRGBO(38, 82, 140, 1),
-                    ),
-                  ),
-                  onTap: () => Get.to(() => const BienImmobilierPage()),
-                ),
-                const Divider(
-                  thickness: 1,
-                  color: Color.fromRGBO(181, 196, 216, 0.65),
-                ),
-                ListTile(
-                  leading: Image.asset(
-                    "assets/images/icons/locataire_logo.png",
-                    width: 48,
-                    height: 48,
-                  ),
-                  title: const Text(
-                    "Locataire",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color.fromRGBO(7, 21, 60, 1),
-                    ),
-                  ),
-                  subtitle: const Text(
-                    "Vous louer une maison, un appartement, des bureaux",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Color.fromRGBO(38, 82, 140, 1),
-                    ),
-                  ),
-                  onTap: () => Get.to(() => const FormulaireCodeMaisonLoyer()),
-                ),
-              ],
+            subtitle: const Text(
+              "Vous possédez ou gérez 1 ou plusieurs biens immobilliers",
+              style: TextStyle(
+                fontSize: 12,
+                color: Color.fromRGBO(38, 82, 140, 1),
+              ),
             ),
-          )
+            onTap: () => Get.to(() => const LoyerProprietaireHomePage()),
+          ),
+          const Divider(
+            indent: 50,
+            thickness: 1,
+            color: Color.fromRGBO(181, 196, 216, 0.65),
+          ),
+          ListTile(
+            leading: Image.asset("assets/images/icons/locataire_logo.png",
+                width: 48),
+            title: const Text(
+              "Locataire",
+              style: TextStyle(
+                fontSize: 15,
+                color: Color.fromRGBO(7, 21, 60, 1),
+              ),
+            ),
+            subtitle: const Text(
+              "Vous louer une maison, un appartement, des bureaux",
+              style: TextStyle(
+                fontSize: 12,
+                color: Color.fromRGBO(38, 82, 140, 1),
+              ),
+            ),
+            onTap: () => Get.to(() => const LoyerLocataireHomePage()),
+          ),
         ],
       ),
     );
